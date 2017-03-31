6bc58606910b28e266bbd67cb5a99e10fe3866c7
TheDekan <ssalenkodd@gmail.com>
2017-03-31 17:57:02 +0300

    Update MakeOutLines.java

--- src/com/salenko/MakeOutLines.java
+++ src/com/salenko/MakeOutLines.java
@@ -5,34 +5,33 @@ import java.util.ArrayList;
 
 public class MakeOutLines {
 
-	public static ArrayList<Rectangle> calculateRectangles(boolean[][] matrixx,
-			int x, int y) {
-		ArrayList<Rectangle> rectangles = new ArrayList<>();
-		for (int i = 0; i < x; i++) {
-			for (int j = 0; j < y; j++) {
-				if (matrixx[i][j] == true) {
-					Rectangle rect = new Rectangle(i - 2, j - 2, 5, 5);
-					if (rectangles.isEmpty()) {
-						rectangles.add(rect);
-					} else {
-						Boolean added = true;
-						while (added) {
-							added = false;
-							for (int ii = 0; ii < rectangles.size(); ii++) {
-								if (rectangles.get(ii).intersects(rect)) {
-									rect.add(rectangles.get(ii));
-									rectangles.remove(rectangles.get(ii));
-									added = true;
-									break;
-								}
-							}
-						}
-						rectangles.add(rect);
-					}
-				}
-			}
-		}
-		return rectangles;
-	}
+    public static ArrayList<Rectangle> calculateRectangles(boolean[][] matrixx, int x, int y) {
+        ArrayList<Rectangle> rectangles = new ArrayList<>();
+        for (int i = 0; i < x; i++) {
+            for (int j = 0; j < y; j++) {
+                if (matrixx[i][j] == true) {
+                    Rectangle rect = new Rectangle(i - 2, j - 2, 5, 5);
+                    if (rectangles.isEmpty()) {
+                        rectangles.add(rect);
+                    } else {
+                        Boolean added = true;
+                        while (added) {
+                            added = false;
+                            for (int ii = 0; ii < rectangles.size(); ii++) {
+                                if (rectangles.get(ii).intersects(rect)) {
+                                    rect.add(rectangles.get(ii));
+                                    rectangles.remove(rectangles.get(ii));
+                                    added = true;
+                                    break;
+                                }
+                            }
+                        }
+                        rectangles.add(rect);
+                    }
+                }
+            }
+        }
+        return rectangles;
+    }
 
 }
